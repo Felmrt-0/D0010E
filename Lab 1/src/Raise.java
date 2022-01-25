8071031b2134ac5ec889ce7998fbd3fc4779e91e
Kyrne <johnnylam0914@gmail.com>
2022-01-25 23:08:13 +0100

    Raise

--- Lab 1/src/Raise.java
+++ Lab 1/src/Raise.java
@@ -1,20 +1,40 @@
 public class Raise {
 
+    public static int recHalfCount;
+    public static int recOneCount;
+
     public static double recRaiseOne(double x, int k) {
         if (k == 0) {
             return 1.0;
         } else {
+            recOneCount++;
             return x * recRaiseOne(x, k - 1);
         }
     }
 
     public static double recRaiseHalf(double x, int k) {
         if (k == 0) {
-            return 1;
+            return 1.0;
         } else if (k % 2 == 0) {
-            return Math.pow(x * Math.floor(k / 2), 2);
+            recHalfCount++;
+            double rec = recRaiseHalf(x, k / 2);
+            return rec * rec;
         } else {
-            return x * Math.pow(x * Math.floor(k / 2), 2);
+            recHalfCount++;
+            double rec = recRaiseHalf(x, k / 2);
+            return x * rec * rec;
+        }
+    }
+
+    public static void main(String[] args) {
+        double x = 1.5;
+        for (int k = 1; k <= 15; k++) {
+            recOneCount = 0;
+            recHalfCount = 0;
+            System.out.println("RecRaiseOne for x = " + x + " and k = " + k + " = " + recRaiseOne(x, k) + ", and took " + recOneCount + " recursive calls");
+            System.out.println("RecRaiseHalf for x = " + x + " and k = " + k + " = " + recRaiseHalf(x, k) + ", and took " + recHalfCount + " recursive calls");
+            System.out.println();
+
         }
     }
 }
