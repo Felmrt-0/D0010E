7fd7437df13f2baa89e9e5660c5230b870d1d029
lam-johnny <60618653+lam-johnny@users.noreply.github.com>
2022-02-10 15:59:22 +0100

    Task 2

--- Labb 3/src/FIFO.java
+++ Labb 3/src/FIFO.java
@@ -1,9 +1,73 @@
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
-public class FIFO{
-    private ArrayList<Object> queue = new ArrayList <Object>();
+public class FIFO {
+    private ArrayList<Object> queue = new ArrayList<Object>();
+    private int maximumSize;
 
-    public int size(){
+    public int size() {
         return queue.size();
     }
+
+    public int maxSize() { 
+        return maximumSize;
+    }
+
+    public boolean isEmpty() {
+        if (queue.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public Object first() throws NoSuchElementException {
+        if (queue.size() == 0) {
+            throw new NoSuchElementException();
+        }
+        return queue.get(0);
+    }
+
+    public boolean equals(Object f) throws ClassCastException {
+        if (f.getClass() != this.getClass()) {
+            throw new ClassCastException();
+        } else if (this.size() == ((FIFO) f).size()) {
+            for (int i = 0; i < this.size(); i++) {
+                if ((this.queue.get(i) == null)) {
+                    if (((FIFO) f).queue.get(i) != null) {
+                        break;
+                    }
+                }
+                if (!this.queue.get(i).equals(((FIFO) f).queue.get(i))) {
+                    break;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        String output = "Queue: ";
+        for (int i = 0; i < size(); i++) {
+            
+            output += "(" + String.valueOf(i) + ") "; //queue.get(i) istället för i?
+        }
+
+        return output;
+    }
+
+    public void add(Object item) {
+        queue.add(item);
+        if (maximumSize < queue.size()) {
+            maximumSize = queue.size();
+        }
+    }
+
+    public void removeFirst() throws NoSuchElementException {
+        if (queue.size() == 0) {
+            throw new NoSuchElementException();
+        }
+        queue.remove(0);
+    }
+
 }
