8df4c44df994a42be52a248a1f5bc32528133594
lam-johnny <60618653+lam-johnny@users.noreply.github.com>
2022-02-11 15:14:17 +0100

    Labb 3 klar

--- Labb 3/src/GraphIO.java
+++ Labb 3/src/GraphIO.java
@@ -3,7 +3,29 @@ import java.io.*;
 import java.util.Scanner;
 
 public class GraphIO {
-    static public void readFile(Graph g, String filename){
-        
+
+    private static int[] x;
+
+    static public void readFile(Graph g, String filename) {
+        System.out.print(filename);
+
+        File data = new File(filename);
+        Scanner scan = null;
+
+        try {
+            scan = new Scanner(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int lines = scan.nextInt();
+
+        for(int i = 0; i < lines; i++){
+            g.addNode(scan.nextInt(), scan.nextInt(), scan.nextInt());
+        }
+
+        while(scan.hasNext()){
+            g.addEdge(scan.nextInt(), scan.nextInt(), scan.nextInt());
+        }
+
     }
 }
