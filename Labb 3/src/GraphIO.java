df0a46da74fb4f1fc8504c213209321eb7cf9e7e
lam-johnny <60618653+lam-johnny@users.noreply.github.com>
2022-02-11 01:15:36 +0100

    GraphIO

---
+++ Labb 3/src/GraphIO.java
@@ -0,0 +1,9 @@
+import java.util.NoSuchElementException;
+import java.io.*;
+import java.util.Scanner;
+
+public class GraphIO {
+    static public void readFile(Graph g, String filename){
+        
+    }
+}
