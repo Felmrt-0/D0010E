7e25f0d6110064d2f68d571d75d3f66455e4a674
lam-johnny <60618653+lam-johnny@users.noreply.github.com>
2022-02-22 21:30:32 +0100

    Create GomokuMain.java

--- Labb 4/lab4/GomokuMain.java
+++ Labb 4/lab4/GomokuMain.java
@@ -3,18 +3,13 @@ package lab4;
 import lab4.data.GomokuGameState;
 import lab4.gui.GomokuGUI;
 
-class GomokuMain {
-    public GomokuMain {
-
-    }
-
+public class GomokuMain {
     public static void main(String[] args) {
         int port = 4000;
-        if (args.length > 1){
-            throw new
-        }
-        else if (args.length == 1 && args =>0){
-            port = args;
+        if (args.length > 1) {
+            throw new IllegalArgumentException();
+        } else if (args.length == 1 && Integer.parseInt(args[0]) >= 0) {
+            port = Integer.parseInt(args[0]);
         }
 
         GomokuClient client = new GomokuClient(port);
